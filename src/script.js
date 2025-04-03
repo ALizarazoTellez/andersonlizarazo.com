55baea1778b3f7f79de5b416be84461ca8c2301c
Anderson Lizarazo Tellez <andersonlizarazo27@gmail.com>
2025-04-02 21:14:18 -0500

    Use IntersectionObserver instead of legacy API

--- src/script.js
+++ src/script.js
@@ -3,13 +3,24 @@
 let mainContent = document.querySelector("main");
 let topbar = document.querySelector(".topbar");
 
-document.querySelector("body").addEventListener("scroll", () => {
-  if (
-    mainContent.getBoundingClientRect().top <=
-    Number.parseInt(window.getComputedStyle(topbar).height) * 3
-  ) {
-    topbar.hidden = false;
-  } else {
-    topbar.hidden = true;
-  }
+const observer = new IntersectionObserver(
+  (elems) => {
+    let showTopbar = false;
+    for (let elem of elems) {
+      if (elem.isIntersecting) {
+        showTopbar = true;
+        topbar.innerText = elem.target.dataset.title;
+      }
+    }
+
+    topbar.hidden = !showTopbar;
+  },
+  {
+    rootMargin: "0px",
+    threshold: 0.85,
+  },
+);
+
+mainContent.querySelectorAll("section").forEach((elem) => {
+  observer.observe(elem);
 });
